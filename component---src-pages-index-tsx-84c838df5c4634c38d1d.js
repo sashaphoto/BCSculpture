470156fce0e2629a9419b805e7bedeca5c1901ee
TheBigSasha <TheBigSasha@users.noreply.github.com>
2022-05-10 16:38:47 +0000

    deployed via Gatsby Publish Action 🎩 for a2162cdc0e453fad59c2ae15d79ce43d25cd8688

---
+++ component---src-pages-index-tsx-84c838df5c4634c38d1d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[691],{8308:function(t,e,r){r.r(e);var a=r(7294);e.default=function(){return a.createElement("iframe",{style:{width:"100vw",height:"100vh",frameBorder:0},src:"https://thebigsasha.github.io/bcsculpture/",title:"BC Sculpture"},"Browser Incompatible")}}}]);
+//# sourceMappingURL=component---src-pages-index-tsx-84c838df5c4634c38d1d.js.map
